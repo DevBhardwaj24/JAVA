b7bb50a6b8a48f97a1d0810203d2d46f851bdc82
DevBhardwaj24 <devb75028@gmail.com>
2026-01-15 12:43:04 +0530

    Package chapter completed

---
+++ Section-17 Packages/AccessModifier.java
@@ -0,0 +1,4 @@
+public class AccessModifier {
+    
+}
+// there are 4 types of access modifier --> default , private , protected , public 
---
+++ Section-17 Packages/CreatingPackage.java
@@ -0,0 +1,6 @@
+public class CreatingPackage {
+    public static void main(String[] args) {
+         
+    }   
+}
+//Creating a package is a whole new thing for me and didnot understand it purely
